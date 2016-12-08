48e31b621272cf113fe65121445bf7c1cecf512e
knightliao <knightliao@gmail.com>
2016-12-08 19:18:53 +0800

    add 注释

--- canalx-core/src/main/java/com/github/knightliao/canalx/core/support/context/ICanalxContext.java
+++ canalx-core/src/main/java/com/github/knightliao/canalx/core/support/context/ICanalxContext.java
@@ -12,13 +12,40 @@ import com.github.knightliao.canalx.core.support.properties.IProperties;
  */
 public interface ICanalxContext extends IProperties {
 
+    /**
+     * 获取 injector plugin name
+     *
+     * @return
+     */
     Set<String> getInjectorPluginName();
 
+    /**
+     * 获取 processor plugin name
+     *
+     * @return
+     */
     Set<String> getProcessorPluginName();
 
+    /**
+     * 获取router plugin name
+     *
+     * @return
+     */
     Set<String> getRouterPluginName();
 
+    /**
+     * 载入
+     *
+     * @throws IOException
+     */
     void load() throws IOException;
 
+    /**
+     * processor 作为数据源
+     *
+     * @param processorName
+     *
+     * @return
+     */
     ICanalxDataRouter getProcessorAsDataSource(String processorName);
 }
